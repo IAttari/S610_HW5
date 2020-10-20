faefbd9af56e2efa4ec24277ce7f10f2ddd65150
IAttari <iattari@iu.edu>
2020-10-20 01:16:26 -0400

    first speed up

--- llr_functions.R
+++ llr_functions.R
@@ -6,7 +6,7 @@ llr = function(x, y, z, omega) {
 compute_f_hat = function(z, x, y, omega) {
   Wz = make_weight_matrix(z, x, omega)
   X = make_predictor_matrix(x)
-  f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+  f_hat = c(1, z) %*% solve(t(X) %*% t(sapply(1:nrow(X), function(i) X[i, ] * Wz[i]))) %*% t(X) %*% matrix(sapply(1:length(y), function(i) y[i] * Wz[i]))
   return(f_hat)
 }
 
@@ -27,8 +27,7 @@ make_weight_matrix = function(z, x, omega){
     } 
     j = j+1
   }
-  Wz = diag(W)
-  return(Wz)
+  return(W)
 }
 
 
